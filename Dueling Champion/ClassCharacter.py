b9acfd73e44706b3ed97c8564e31403c4b6d57b9
ArchPhoenix <schmitty500@gmail.com>
2014-12-07 16:02:53 -0600

    Dueling Champion
    Initial Commit

---
+++ Dueling Champion/ClassCharacter.py
@@ -0,0 +1,44 @@
+class Character(object):
+
+#Character constructor    
+    def __init__ (self, statLine):
+        self.__name = statLine[0]
+        self.__ID = statLine[1]
+        self.__level = statLine[2]
+        self.__health = statLine[3]
+        self.__totalHealth = statLine[3]
+        self.__strength = statLine[4]
+
+#Methods to receive specific class information
+    def getName (self):
+        return self.__name
+    def getID (self):
+        return self.__ID
+    def getLevel (self):
+        return self.__level
+    def getHealth (self):
+        return self.__health
+    def getTotalHealth (self):
+        return self.__totalHealth
+    def getStrength (self):
+        return self.__strength
+    
+#Methods to change stats
+    def changeTotalHealth(self, healthUpgrade):
+        self.__totalHealth = self.__totalHealth + healthChange
+        self.__health = self.__totalHealth
+        
+    def changeHealth(self, damage):
+        self.__health = self.__health - damage
+        
+
+#Print methods
+    def __str__ (self):
+        
+        return self.__name
+        #return str(self.__name) + "," + str(self.__level) + "," + str(self.__health) + "," + str(self.__totalHealth) + "," + str(self.__strength)
+
+    def __repr__ (self):
+        
+        return self.__name
+        #return str(self.__name) + "," + str(self.__level) + "," + str(self.__health) + "," + str(self.__totalHealth) + "," + str(self.__strength)
---
+++ Dueling Champion/TheoryTesting.py
@@ -0,0 +1,16 @@
+i = 1
+d = {"chair": "arm"}
+print (d["chair"])
+d = {"so" + "fa": "cushion"}
+print (d["sofa"])
+d["enemy" + str(i)] = "bowser"
+print (d["enemy1"])
+print (d)
+
+
+
+i = 1
+k = 2
+#I want to assign enemy1 to bowser and enemy2 to bowser junior WITHOUT typing the values 1 or 2
+list1 = ["enemy" + str(1), "enemy2"]
+print (list1)
